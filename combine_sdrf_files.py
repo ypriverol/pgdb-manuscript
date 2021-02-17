ed6c63e5822c66104c6caff80e70760da6060085
husensofteng <husensofteng@gmail.com>
2021-02-17 13:33:43 +0100

    combine sdrfs into one tsv file

---
+++ combine_sdrf_files.py
@@ -0,0 +1,60 @@
+'''
+Created on 17 Feb 2021
+
+@author: husen
+'''
+
+import sys
+import glob
+import argparse
+
+def parse_commandline_args():
+    """
+    read command line arguments or set the default values
+    """
+    parser = argparse.ArgumentParser(description='Combine sdrf files into one tsv file')
+    parser.add_argument('-p', '--path_to_datasets', deafuilt = 'multiomics-configs/datasets/proteogenomics-samples/sample-specific', 
+                        help= "Path to directory containing tsv files, each for a sample dataset")
+    parser.add_argument('-o', '--output_file', default = 'combined_sdrfs.tsv', help= "output file name")
+    
+    return parser.parse_args(sys.argv[1:])
+
+def read_datasets(datasets, output):
+    
+    column_indices = {}
+    
+    for dataset in datasets:
+        with open(dataset, 'r') as ds:
+            dataset_name = dataset.split('/')[-1]
+            header = ds.readline().strip().lower().split('\t')
+            if  len(header)>2:
+                for i, col in enumerate(header):
+                    column_indices[col] = 1
+    
+    with open(output, 'w') as out:
+        out.write('\t'.join(column_indices.keys()) + '\n')
+        for dataset in datasets:
+            with open(dataset, 'r') as ds:
+                header = ds.readline().strip().lower().split('\t')
+                for col in column_indices.keys():
+                    if col in header:
+                        column_indices[col]= header.index(col)
+                    else:
+                        column_indices[col] = 'NA'
+                
+                for l in ds.readlines():
+                    out_sl = []
+                    sl = l.strip().split('\t')
+                    for col,i in column_indices.items():
+                        if i !='NA':
+                            out_sl.append(sl[i])
+                        else:
+                            out_sl.append('NA')
+                    out.write('\t'.join(out_sl) + '\n')
+              
+    
+if __name__ == '__main__':
+    args = parse_commandline_args()
+    datasets = glob.glob(args.path_to_datasets + '/*.tsv')
+    read_datasets(datasets, output = args.output_file)
+    
