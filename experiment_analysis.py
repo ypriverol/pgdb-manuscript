2274a125a2fad0aa4a2a40bb875412d8a1af0f16
ypriverol <ypriverol@gmail.com>
2021-02-22 00:41:01 +0000

    minor changes

---
+++ experiment_analysis.py
@@ -0,0 +1,123 @@
+import os
+
+import pandas as pd
+import glob
+from typing import Any, Dict, List, Set
+import pyopenms as oms
+
+
+def ms_run_from_path(path: str, pattern: str):
+  """
+  Get MSRun from path
+  """
+  head, tail = os.path.split(path)
+  tail = tail.replace(pattern, "")
+  return tail
+
+def getIDQuality( idxml_file : str ):
+  pro_ids = list()
+  pep_ids = list()
+
+  oms_id = oms.IdXMLFile()
+  oms_id.load(idxml_file, pro_ids, pep_ids)
+
+  psm_count: int = 0
+  protein_evidence_count: int = 0
+  spectrum_count = 0
+
+  report = {}
+
+  peptides_allhits: Set[str] = set()
+  peptides: Set[str] = set()
+  proteins: Set[str] = set()
+
+  for pepi in pep_ids:
+    if not pepi.empty():
+      # TODO if not decoy and not under threshold
+      spectrum_count += 1
+      psm_count += len(pepi.getHits())
+      for psm in pepi.getHits():
+        peptides_allhits.add(psm.getSequence().toString())
+      if pepi.getHits():
+        peptides.add(pepi.getHits()[0].getSequence().toString())
+
+  for proid in pro_ids:
+    protein_evidence_count += len(proid.getHits())
+    for p in proid.getHits():
+      proteins.add(p.getAccession())
+
+  report['protein evidences'] = protein_evidence_count
+  report['identified proteins'] = len(proteins)
+  report['number of PSM'] = psm_count
+  report['peptide spectra'] = spectrum_count
+  report['unique peptide sequences'] = len(peptides)
+  return report
+
+def create_df_metrics(props: dict, ms_run: str, step: str, sample: str):
+  props['process step'] = step
+  props['ms_run'] = ms_run
+  props['sample'] = sample
+  return pd.DataFrame().append(props, ignore_index=True)
+
+project_path = "/Users/yperez/work/proteogenomics/PXD005946-Sample-19-msgf-comet-bayesian"
+project = 'PXD005946-Sample-19'
+
+consensus_ids = 'consensus_ids'
+dbs = 'dbs'
+ids = 'ids'
+logs = 'logs'
+proteomics_lfq = 'proteomics_lfq'
+raw_ids = 'raw_ids'
+
+triqler_file = 'out_triqler.tsv'
+mztab = 'out.mzTab'
+
+consensus_pattern = '_consensus.idXML'
+consensus_fdr_pattern = '_consensus_fdr.idXML'
+filter_pattern = '_consensus_fdr_filter.idXML'
+
+comet_percolator_pattern = '_comet_idx_feat_perc.idXML'
+msgf_percolator_pattern = '_msgf_idx_feat_perc.idXML'
+
+# Final results
+report_peptides_df = pd.DataFrame(
+  pd.read_csv("{}/{}/{}".format(project_path, proteomics_lfq, triqler_file), sep='\t', header=0))
+print(report_peptides_df.head(2))
+
+runs = report_peptides_df['run'].nunique()
+num_peptides = len(report_peptides_df)
+avg = num_peptides / runs
+
+print("Number of peptides: {}, Avg. Peptides per run: {}".format(num_peptides, avg))
+file_names = glob.glob("{}/{}/*{}".format(project_path, ids, consensus_fdr_pattern))
+
+ms_runs = [ms_run_from_path(x, consensus_fdr_pattern) for x in file_names]
+print(ms_runs)
+
+
+# Get consensus filter and consensus
+frames = []
+for sample in ms_runs:
+
+  consensus_path = "{}/{}/{}{}".format(project_path, ids, sample, consensus_fdr_pattern)
+  if os.path.exists(consensus_path):
+    frames.append(create_df_metrics(getIDQuality(consensus_path), sample, 'consensus FDR', project))
+
+  consensus_path_filter = "{}/{}/{}{}".format(project_path, ids,sample, filter_pattern)
+  if os.path.exists(consensus_path_filter):
+    frames.append(create_df_metrics(getIDQuality(consensus_path_filter), sample, 'consensus Filter', project))
+
+  comet_path = "{}/{}/{}{}".format(project_path, raw_ids, sample, comet_percolator_pattern)
+  if os.path.exists(comet_path):
+    frames.append(create_df_metrics(getIDQuality(comet_path), sample, 'comet', project))
+
+  msgf_path = "{}/{}/{}{}".format(project_path, raw_ids, sample, msgf_percolator_pattern)
+  if os.path.exists(msgf_path):
+    frames.append(create_df_metrics(getIDQuality(msgf_path), sample, 'msgf', project))
+
+result = pd.concat(frames)
+result.to_csv('results.tsv', sep='\t')
+
+
+
+
