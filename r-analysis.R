0d649624517c45fd63617eea55c63579c8f24b82
ypriverol <ypriverol@gmail.com>
2021-04-03 15:53:51 +0100

    number of novel peptides per cell line.

--- r-analysis.R
+++ r-analysis.R
@@ -32,13 +32,23 @@ intensity_data <- intensity_data %>% mutate(intensity_norm = normalit(log_intens
 # Correct one cell-line annotation
 intensity_data <- intensity_data %>% mutate(condition = ifelse(as.character(condition) == "A-549 cell", "A549", as.character(condition)))
 
+# Number of movel peptides per cell line
+intensity_data_filer <- intensity_data[!duplicated(intensity_data[c("condition", "sequence")]),]
+novel_data <- intensity_data_filer %>% select(condition, Class)
 
+grouped_data <- aggregate(novel_data, by=list(novel_data$condition, novel_data$Class), FUN=length);
+names(grouped_data)[1] <- "cell_lines"
+names(grouped_data)[3] <- "Count"
+names(grouped_data)[4] <- "2"
+names(grouped_data)[2] <- "Class"
+grouped_data <- grouped_data %>% select(cell_lines, Class, Count)
+grouped_data <- grouped_data %>% filter(Class != "contaminant")
+grouped_data <- grouped_data %>% filter(Class != "canonical")
 
+# make grouped bar plot
+p <- ggplot(grouped_data) + geom_bar(aes(x = cell_lines, y = Count, fill = Class), stat = "identity", position = "dodge")  + theme_minimal() + theme(axis.text.x = element_text(angle = 90)) + labs(x = "Cell lines", y = "Number of peptides")
+ggsave(file="peptide-class.svg", plot=p, width=10, height=8)
 
-
-
-
-sapply(split(intensity_data$aa_length, intensity_data$Class), mean)
 
 
 intensity_data %>% ggplot(aes(x=Sample, y=intensity_norm, fill=condition)) + geom_boxplot() + xlab("Cell lines") + theme(axis.text.x = element_text(angle = 45, hjust = 1))
