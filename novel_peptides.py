406438fe3f7bb3bc24b97c26538f5c184ad6a3e7
ypriverol <ypriverol@gmail.com>
2021-10-19 17:08:07 +0100

    number of novel peptides per cell line.

--- novel_peptides.py
+++ novel_peptides.py
@@ -5,6 +5,9 @@ from typing import List
 import requests
 import re
 
+from ratelimit import limits
+
+FIFTEEN_MINUTES = 100
 
 def print_help():
   """
@@ -15,12 +18,13 @@ def print_help():
   click.echo(ctx.get_help())
   ctx.exit()
 
+@limits(calls=15, period=FIFTEEN_MINUTES)
 def check_peptides_nextprot(peptide_list: List):
   """
   Check if the peptide sequence is in NextProt
   """
   for sequence in peptide_list:
-    r = requests.get('https://api.nextprot.org/entries/search/peptide.json?peptide={}&no-variant-match=false'.format(sequence))
+    r = requests.get('https://api.nextprot.org/entries/search/peptide.json?peptide={}&no-variant-match=true'.format(sequence))
     if r.status_code == 200:
       data = r.json()
       if len(data) > 0:
@@ -30,6 +34,7 @@ def check_peptides_nextprot(peptide_list: List):
 
   return peptide_list
 
+@limits(calls=15, period=FIFTEEN_MINUTES)
 def check_peptides_gpmdb(peptide_list: List):
   """
   Check the peptides in GPMDB and get the number of observations.
@@ -38,9 +43,8 @@ def check_peptides_gpmdb(peptide_list: List):
     r = requests.get('http://rest.thegpm.org/1/peptide/count/seq={}'.format(sequence))
     data = r.json()
     peptide_list[sequence]['gpmdb'] = data[0]
-    print("Peptide {} - GPMDB {}".format(sequence, data[0]))
+    # print("Peptide {} - GPMDB {}".format(sequence, data[0]))
   return peptide_list
-
 
 def count_observations(data_proxi) -> int:
   count = 0
@@ -49,11 +53,12 @@ def count_observations(data_proxi) -> int:
       count = count + int(key['countPSM'])
   return count
 
-
+@limits(calls=15, period=FIFTEEN_MINUTES)
 def check_peptides_proxi(peptide_list: List):
   """
   Count the number of observations in Proxi entry points MAssIVE or PeptideAtlas
   """
+  print("Peptide\tGPMDB\tPeptideAtlas\tMassiVE")
   for sequence in peptide_list:
     massive_url = 'http://massive.ucsd.edu/ProteoSAFe/proxi/v0.1/peptidoforms?resultType=compact&peptideSequence={}'.format(sequence)
     result = requests.get(massive_url)
@@ -64,6 +69,13 @@ def check_peptides_proxi(peptide_list: List):
     peptide_atlas = 'http://www.peptideatlas.org/api/proxi/v0.1/peptidoforms?resultType=compact&peptideSequence={}'.format( sequence)
     data = requests.get(peptide_atlas).json()
     peptide_list[sequence]['peptideatlas'] = count_observations(data)
+
+    r = requests.get('http://rest.thegpm.org/1/peptide/count/seq={}'.format(sequence))
+    data = r.json()
+    peptide_list[sequence]['gpmdb'] = data[0]
+
+    print("{}\t{}\t{}\t{}"
+          .format(sequence, peptide_list[sequence]['gpmdb'], peptide_list[sequence]['peptideatlas'], peptide_list[sequence]['massive']))
 
   return peptide_list
 
@@ -87,11 +99,12 @@ def main(peptides_file: str):
     for cnt, line in enumerate(fp):
       if len(line) > 0:
         line = clean_peptide(line)
-        peptide = {'sequence': line, 'gpmdb': 0, 'peptideatlas': 0, 'pride': 0, 'massive': 0, 'nextprot':'', 'uniprot':''}
-        peptides[line] = peptide
+        if len(line) > 8:
+          peptide = {'sequence': line, 'gpmdb': 0, 'peptideatlas': 0, 'pride': 0, 'massive': 0, 'nextprot':'', 'uniprot':''}
+          peptides[line] = peptide
 
-  peptides = check_peptides_nextprot(peptides)
-  peptides = check_peptides_gpmdb(peptides)
+  #peptides = check_peptides_nextprot(peptides)
+  # peptides = check_peptides_gpmdb(peptides)
   peptides = check_peptides_proxi(peptides)
 
   for sequence in peptides:
